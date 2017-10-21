ee6ede442f308ecaf1f98154ea164617a16df687
mario-serna <31573347+mario-serna@users.noreply.github.com>
2017-10-21 18:39:09 -0500

    Add ng2-materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MzToastService } from 'ng2-materialize';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private toastService: MzToastService) {
+  }
+
+  showToast() {
+    this.toastService.show('Materialize works!', 4000, 'green');
+  }
 }
